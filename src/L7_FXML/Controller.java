f9fcbd5c652d791385724db2a8e9223064e2d568
Ahmed Badawy <admin@ahmed-badawy.com>
2016-02-09 13:15:39 +0200

    updating the fxml layout

--- src/L7_FXML/Controller.java
+++ src/L7_FXML/Controller.java
@@ -4,12 +4,19 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 public class Controller implements Initializable {
+	
+	public Button btn1;
 	
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("View is now loaded!");
+    }
+    
+    public void btn_clicked(){
+    	System.out.println("Button Was Clicked !");
     }
 
     
